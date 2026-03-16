29bbe46b04312bda557c0cfcb3c0fba556327a0d
even182 <49551205+even182@users.noreply.github.com>
2026-03-16 21:07:14 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.set_page_config(page_title="Richard 投資儀表板", layout="wide")
+st.set_page_config(page_title="投資儀表板", layout="wide")
 
 st.title("投資儀表板")
 st.write("請從左側選單選擇要查看的頁面：Richard / Family")
